2e3b40465368c572ab3e32d48ffb21b15023e964
chenfanda <fanda.hk2013@gmail.com>
2024-08-15 13:25:51 +0800

    5

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,29 @@
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "normalize.css/normalize.css"
-import "./globals.css"
+import type { Metadata } from 'next'
+import StyledComponentsRegistry from '../lib/registry'
+import { Inter } from 'next/font/google'
+import 'normalize.css/normalize.css'
+import './globals.css'
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-	title: "Front-end Engineering Challenge",
-	description: "Onsite Coding Challenge",
+  title: 'Front-end Engineering Challenge',
+  description: 'Onsite Coding Challenge'
 }
 
 export default function RootLayout({
-	children,
+  children
 }: Readonly<{
-	children: React.ReactNode
+  children: React.ReactNode
 }>) {
-	return (
-		<html lang="en">
-			<head>
-				<link rel="icon" href="/images/favicon.ico" />
-			</head>
-			<body className={inter.className}>{children}</body>
-		</html>
-	)
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/images/favicon.ico" />
+      </head>
+      <body className={inter.className}>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </body>
+    </html>
+  )
 }
