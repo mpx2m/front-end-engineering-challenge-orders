0b2052a25e2598815b98098e2e9b537ed9da453f
chenfanda <fanda.hk2013@gmail.com>
2024-08-15 18:27:58 +0800

    feat: add zustand

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import StyledComponentsRegistry from '@/lib/styled-components-registry'
+import StyledComponentsRegistry from './_lib/registry'
 import { Inter } from 'next/font/google'
 import 'normalize.css/normalize.css'
 import './globals.css'
