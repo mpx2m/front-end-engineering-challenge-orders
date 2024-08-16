d61b5210f9d99d583b81e3602774daea0357538e
chenfanda <fanda.hk2013@gmail.com>
2024-08-16 20:01:48 +0800

    feat: fix error

--- app/_components/menu-item.tsx
+++ app/_components/menu-item.tsx
@@ -2,16 +2,16 @@ import Link from 'next/link'
 import styled from 'styled-components'
 import { theme } from '@/components/theme'
 
-export const MenuItem = styled(Link)<{ isActive?: boolean }>`
+export const MenuItem = styled(Link)<{ $isActive?: boolean }>`
   text-decoration: none;
   display: block;
   padding: 16px;
   color: ${theme.textColorWhite};
-  cursor: ${props => (props.isActive ? 'default' : 'pointer')};
+  cursor: ${props => (props.$isActive ? 'default' : 'pointer')};
   background-color: ${props =>
-    props.isActive ? theme.highlight : 'transparent'};
+    props.$isActive ? theme.highlight : 'transparent'};
   &:hover {
     background-color: ${props =>
-      props.isActive ? theme.highlight : theme.hoverButton};
+      props.$isActive ? theme.highlight : theme.hoverButton};
   }
 `
