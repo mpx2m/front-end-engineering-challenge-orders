13402948fc98d524164af7b4d7af432c17c01f30
chenfanda <fanda.hk2013@gmail.com>
2024-08-17 12:21:50 +0800

    feat: add display

--- app/page.tsx
+++ app/page.tsx
@@ -18,24 +18,34 @@ const Page: React.FC = () => {
         textAlign: 'center'
       }}
     >
-      <section>
-        Count: {count}
-        <hr />
+      <div>
+        <h1>Custom Table with External Data</h1>
+        <section>
+          <span>Status: {isConnected ? 'connected' : 'disconnected'}</span>
+          <span style={{ marginLeft: 20 }} />
+          <span>Transport: {transport}</span>
+          <span style={{ marginLeft: 20 }} />
+          <span>Order Count: {10000}</span>
+          <span style={{ marginLeft: 20 }} />
+          <span>Customer Count: {20000}</span>
+          <span style={{ marginLeft: 20 }} />
+          <span>Total Amount ($): {30000}</span>
+        </section>
+      </div>
+      <hr />
+      <div>
+        <section>Count: {count}</section>
         <button type="button" onClick={() => void incrementCount()}>
           Increment Count
         </button>
         <button type="button" onClick={() => void decrementCount()}>
           Decrement Count
         </button>
-      </section>
-      <section>
-        <p>Status: {isConnected ? 'connected' : 'disconnected'}</p>
-        <p>Transport: {transport}</p>
-      </section>
-      <section style={{ padding: '20px' }}>
-        <h1>Custom Table with External Data</h1>
+      </div>
+
+      <div style={{ marginTop: '20px' }}>
         <Table dataSource={dataSource} columns={columns} rowKey="id" />
-      </section>
+      </div>
     </div>
   )
 }
